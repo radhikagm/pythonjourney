757d45f5d39e4f7e2ee21cf2faa5b8a1a36c3376
radhikagm <radhikagmanohar@gmail.com>
2016-11-29 15:35:40 -0500

    Update prog_03_sum_of_digits.py

--- prog_03_sum_of_digits.py
+++ prog_03_sum_of_digits.py
@@ -4,9 +4,9 @@ N = int(input("Please Enter any Number: "))
 Sum = 0
  
 while(N > 0):
-    last_digit = N % 10
+    last_digit = (N % 10)
     Sum = Sum + last_digit
-    N = N //10
+    N = (N // 10)
     
 print("Sum of the digits of the given number is", Sum)
  
