cfdf055317298447b009229523ce82ad5be8f5ec
radhikagm <radhikagmanohar@gmail.com>
2016-11-29 12:57:52 -0500

    Add files via upload

---
+++ prog_02_print_n_num.py
@@ -0,0 +1,11 @@
+# WAP to print n numbers when n is given by the user - For Loop
+
+print("enter the number")
+n = int(input())
+
+print("Printing numbers 1 to n")
+
+for x in range(1,n+1):
+  print(x)
+  
+  
