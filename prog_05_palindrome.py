3bf4e0543938fed9287d0c01fc9afda88bbec8ba
radhikagm <radhikagmanohar@gmail.com>
2016-11-29 21:14:21 -0500

    Update prog_05_palindrome.py

--- prog_05_palindrome.py
+++ prog_05_palindrome.py
@@ -1,20 +1,28 @@
 #!/Users/Radhika/Documents/Udemy/anaconda/bin/python
 
+#Using functions 
 N = int(input("Please Enter any Number: "))
+  
+def is_palindrome(N):
+  Number = N
 
-Number = N
+  Reverse = 0
 
-Reverse = 0
-
-while(N > 0):
-    last_digit = (N % 10)
-    Reverse = (Reverse *10) + last_digit
-    N = (N // 10)
+  while(N > 0):
+      last_digit = (N % 10)
+      Reverse = (Reverse *10) + last_digit
+      N = (N // 10)
     
-print("Reverse of entered number is", Reverse)
+  print("Reverse of entered number is", Reverse)
  
-if (Reverse == Number):
-  print("Number is a palindrome")
-else:
-  print("Number is not a palindrome")
+  if (Reverse == Number):
+    print("Number is a palindrome")
+  else:
+    print("Number is not a palindrome")
+
+#end of function
+
+#Function Call
+is_palindrome(456)
+is_palindrome(454)
 
